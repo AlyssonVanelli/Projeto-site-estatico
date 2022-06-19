0fa4171e923ec1549de8386e955eb44e032a7c30
Alysson <aharon_curitiba@hotmail.com>
2022-06-19 00:52:26 -0300

    front end

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
-const bodyParser = require('body-parser');
 const express = require('express');
+var bodyParser = require('body-parser')
+
 const path = require('path');
 
 const app = express();
 
-app.use( bodyParser.json() );
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, '/pages'));
 
-app.get('/', (req, res) => {
 
-    if(req.query.busca == null) {
-        res.send('Home')
+app.get('/',(req,res)=>{
+    
+    if(req.query.busca == null){
+        res.render('home',{});
     }else{
-        res.send('Você buscou: ' + req.query.busca);
+        res.render('busca',{});
     }
+
+  
+});
+
+
+app.get('/:slug',(req,res)=>{
+    //res.send(req.params.slug);
+    res.render('single',{});
 })
 
-app.get('/:slug', (req, res) => {
-    res.send(req.params.slug)
-})
+
 
 app.listen(5000,()=>{
-    console.log('server rodando')
+    console.log('server rodando!');
 })
